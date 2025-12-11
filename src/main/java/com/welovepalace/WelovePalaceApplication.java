74c08ecd8c81eb15dcdf3f4ef59ed6b5800fcef5
MarwanW3 <marwan.warsame@icloud.com>
2025-12-11 12:05:08 +0100

    Klar springboot/gradle

---
+++ src/main/java/com/welovepalace/WelovePalaceApplication.java
@@ -0,0 +1,12 @@
+package com.welovepalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelovePalaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelovePalaceApplication.class, args);
+    }
+}
